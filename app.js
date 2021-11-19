44c086726135063271017039f18e2c8fd1b439e6
Dalen Ledingham <dalen.ledingham@gmail.com>
2021-11-18 19:23:21 -0600

    Complete and delete buttons now working

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-let inputField = document.getElementById("new-todo");
-let createBtn = document.getElementById("create-btn");
-let todoList = document.getElementById("todo-list");
+let inputField = document.querySelector(".new-todo");
+let createBtn = document.querySelector(".create-btn");
+let todoList = document.querySelector(".todo-list");
 
 createBtn.addEventListener("click", createTodo);
+todoList.addEventListener("click", buttonCheck);
 
 function createTodo(event) {
     event.preventDefault();
@@ -10,20 +11,32 @@ function createTodo(event) {
         return null;
     }
     let todoDiv = document.createElement("div");
-    todoDiv.className = "todo-item";
+    todoDiv.classList.add("todo-item");
     let todoText = document.createElement("li");
     todoText.innerText = inputField.value;
     todoDiv.appendChild(todoText);
 
     let completeBtn = document.createElement("span");
-    completeBtn.className = "complete-btn";
-    completeBtn.innerHTML = "<img src='images/complete.png' alt='complete'>";
+    completeBtn.innerHTML = "<img src='images/complete.png' alt='complete' class='complete-btn'>";
     todoDiv.appendChild(completeBtn);
     
     let deleteBtn = document.createElement("span");
-    deleteBtn.className = "delete-btn";
-    deleteBtn.innerHTML = "<img src='images/delete.png' alt='delete'>";
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.innerHTML = "<img src='images/delete.png' alt='delete' class='delete-btn'>";
     todoDiv.appendChild(deleteBtn);
 
-    todoList.appendChild(todoDiv); 
+    todoList.appendChild(todoDiv);
+    inputField.value = "";
+}
+
+function buttonCheck(event) {
+    let item = event.target;
+    if (item.classList[0] === "delete-btn") {
+        let todo = item.parentElement.parentElement; // item (button) --> span --> div
+        todo.remove();
+    }
+    if (item.classList[0] === "complete-btn") {
+        let todo = item.parentElement.parentElement; // item (button) --> span --> div
+        todo.classList.toggle("completed");
+    }
 }
